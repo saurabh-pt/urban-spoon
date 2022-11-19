25437920f67e9a3618966a7e3866e34f6e7bf296
Saurabh Tripathi <saurav9048@gmail.com>
2022-11-19 13:20:11 +0530

    my first Git commit

---
+++ command-line.py
@@ -0,0 +1,6 @@
+import sys
+
+if __name__ == "__main__":
+    print("This script is being executed using the `python` interpreter")
+    print("The arguments to this script are:")
+    print(sys.argv)
---
+++ hello-world.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+#perform calculations
+x = 2 + 3
+print('The value of x is {}'.format(x))
+
+#import a module
+import math
+y = 2.3
+print('The number {} can be rounded up to {}'.format(y, math.ceil(y)))
